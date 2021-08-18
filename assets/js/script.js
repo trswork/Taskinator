d8e6973bb0822307e1d08e1f753f5755ea732dd2
trswork <87744214+trswork@users.noreply.github.com>
2021-08-17 21:10:37 -0400

    add ability to submit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,22 @@
-var formEl = document.querySelector("#task-form");;
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 
-var createTaskHandler = function(event) {
+var createTaskHandler = function (event) {
     event.preventDefault();
 
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
     console.log(taskTypeInput);
 
+// check if input values are empty strings
+if (!taskNameInput || !taskTypeInput) {
+    alert("You need to fill out the task form!");
+    return false;
+  }
 
+  formEl.reset();
+  
     // create list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
@@ -18,15 +25,15 @@ var createTaskHandler = function(event) {
 var taskInfoEl = document.createElement("div");
 
 // give it a class name
-listItemEl.className = "task-info";
+taskInfoEl.className = "task-info";
+
 // add HTML content to div
-listItemEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
+taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
 
 listItemEl.appendChild(taskInfoEl);
 
 // add entire list item to list
 tasksToDoEl.appendChild(listItemEl);
-console.dir(listItemEl);
   };
 
   formEl.addEventListener("submit", createTaskHandler);
